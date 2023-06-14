e26481c3a72d305fbce887afe9a7afc9a74b5240
Manav Malhotra <110531978+manavmalhotra123@users.noreply.github.com>
2023-06-14 10:18:08 +0530

    Add files via upload

---
+++ arrays/arrays1.cpp
@@ -0,0 +1,40 @@
+
+// time complexity is the amount of time taken to as a fincton of length of input 
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cin >> n;
+    for (size_t i = 0; i < n; i++)
+    {
+        
+        cout<<i<<endl;
+        i = i;        
+    }
+    
+    return 0;
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+    int a = 0,b = 0,m,n;
+    cin >>m>>n;
+    for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++)
+        {
+            cout << "Hi1"<<endl;
+        }
+    }
+
+    for (int i = 0; i < n; i++){
+        cout << "Hi2"<<endl;
+    }
+    return 0;
+}
