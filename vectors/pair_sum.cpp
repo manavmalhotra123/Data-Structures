3c336042c8b44db978d6f6c425d7ef784973c900
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-06-15 11:32:43 +0530

    pair sum question

---
+++ vectors/pair_sum.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void print_vector(vector<int> v){
+    cout<<"Your vector is "<<endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    
+}
+
+
+int main(){
+    vector<int> v = {1,3,5,7};
+    vector<int> v1 = {2,4,6};
+    print_vector(v1);
+    print_vector(v);
+
+    int sum = 9;
+
+    int count = 0;
+
+    // find a pair that will form 9 as it's sum 
+    for (int i = 0; i < v.size(); i++)
+    for (int j = 0; j < v1.size(); j++)
+    {
+        if (v[i] + v1[j] == sum)
+        {
+            count ++;
+            cout<<"Pair "<<count<<" : "<<v[i]<<" and "<<v1[j]<<endl;
+        }
+        
+    }
+
+return 0;
+
+    }
