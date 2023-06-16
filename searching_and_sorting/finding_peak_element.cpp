2ab1b25ace504e767bb6d501608c565f0c3cc6d7
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-06-16 22:04:05 +0530

    peak founded successfully

---
+++ searching_and_sorting/finding_peak_element.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void findPeakElement(vector<int>& nums) {
+    int start = 0;
+    int end = nums.size() - 1;
+
+    while (start < end) {
+        int mid = start + (end - start) / 2;
+
+        if (nums[mid] < nums[mid + 1]) {
+            start = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+
+    cout<<"Peak: "<<end<<endl;
+}
+
+
+void find_peak(vector<int> &v){
+    int maxi = -1;
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (maxi < v[i])
+        {
+            maxi = v[i];
+        }
+        
+    }
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i] == maxi)
+        {
+            cout<<"location of peak element "<<i<<endl;
+        }
+        
+    }
+    
+}
+ 
+
+int main() {
+    vector<int> mountain = {0, 10, 5, 2};
+
+    findPeakElement(mountain);
+
+    find_peak(mountain);
+
+
+    return 0;
+}
--- searching_and_sorting/finding_the_missing_number.cpp
+++ searching_and_sorting/finding_the_missing_number.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void finding_sum_till_largest_number_then_subtrac_from_sum_of_actual_number(vector<int>& v){
+void finding_sum_till_largest_number_then_subtract_from_sum_of_actual_number(vector<int>& v){
 
 int maxi = -1;
 for (int i = 0; i < v.size(); i++)
@@ -40,6 +40,6 @@ cout << "The missing element is "<<missing_element << endl;
 int main(int argc, char const *argv[])
 {
     vector<int> v = {1,2,3,4,5,6,7,8,9,11};
-    finding_sum_till_largest_number_then_subtrac_from_sum_of_actual_number(v);
+    finding_sum_till_largest_number_then_subtract_from_sum_of_actual_number(v);
     return 0;
 }
