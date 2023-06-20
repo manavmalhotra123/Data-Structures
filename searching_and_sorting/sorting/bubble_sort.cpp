c602c238a43b70b801c4d7a7fcbc17e32ff7cf85
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-06-20 15:53:06 +0530

    Bubble sort with step by step implementation as result in output

--- searching_and_sorting/sorting/bubble_sort.cpp
+++ searching_and_sorting/sorting/bubble_sort.cpp
@@ -1,38 +1,43 @@
-// BUBBLE SORT 
+// BUBBLE SORT
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-
-void print_vector(vector<int> &v){
+void print_vector(vector<int> &v)
+{
     for (int i = 0; i < v.size(); i++)
     {
-        cout<<v[i]<<" ";
+        cout << v[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void bubble_sort(vector<int> &v){
+void bubble_sort(vector<int> &v)
+{
+    int round = 1;
     for (int i = 0; i < v.size(); i++)
     {
-        for (int j = i+1; j < v.size(); j++)
+        cout<<"Round "<<round<<endl;
+        for (int j = i + 1; j < v.size(); j++)
         {
             if (v[i] > v[j])
             {
+                cout<<"Swap needed between "<<v[i]<<" and "<<v[j]<<endl;
                 swap(v[i], v[j]);
             }
-            
+            print_vector(v);
         }
-        
+        round ++;
+        cout<<endl;
     }
-    
 }
 
-int main(){
-    vector<int> v = {5,4,3,2,1};
+int main() 
+{
+    vector<int> v = {5, 4, 3, 2, 1};
     print_vector(v);
     bubble_sort(v);
     print_vector(v);
