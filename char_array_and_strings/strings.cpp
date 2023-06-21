0b007c533bf8b150c68c98c1e252193f0cb270c0
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-06-21 15:19:58 +0530

    Basic char vector initialisation syntax

--- char_array_and_strings/strings.cpp
+++ char_array_and_strings/strings.cpp
@@ -3,8 +3,43 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+// function to print the values of vector
+void print_char(const vector<char>& c)
 {
+    for (int i = 0; i < c.size(); i++)
+    {
+        cout << c[i] << " ";
+    }
+    cout << endl;
+}
+
+// function to input the character vector
+void input_char(vector<char>& c)
+{
+    int n;
+    cout << "Enter the number of characters you want to add to the vector: ";
+    cin >> n;
     
+    cout << "n = " << n << endl;
+    
+    c.resize(n); // Resize the vector to accommodate 'n' elements
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> c[i];
+    }
+}
+
+int main()
+{
+    // hardcoding the value of character array
+    vector<char> v = {'a', 'b', 'c', 'd'};
+    print_char(v);
+
+    vector<char> d;
+    // input the values of character array
+    input_char(d);
+    print_char(d);
+
     return 0;
 }
