5fde34f101fa3608246708a340586e80f1799279
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-06-24 16:03:06 +0530

    string basics

---
+++ char_array_and_strings/string1.cpp
@@ -0,0 +1,28 @@
+// String is a Data Type of C++
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+    std::string input;
+
+
+    std::cout << "Enter a string: ";
+    // Taking string as input
+    std::cin >> input;
+
+
+
+    
+    
+    string output = "manav";
+    cout<<output<<std::endl;
+    output.push_back('@');
+    cout<<output<<std::endl;
+
+    return 0;
+}
+
+
+// 01:15:16
