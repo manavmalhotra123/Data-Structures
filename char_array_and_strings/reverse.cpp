0806738e9be60f026defeeeebb6297932ddc8c9b
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-06-24 13:56:07 +0530

    reverse of string using algorithm library as well as user defined function

---
+++ char_array_and_strings/reverse.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void print_vector(vector<char> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+
+
+void reverse_string(vector<char> &v){
+    int start = 0;
+    int end = v.size()-1;
+    while (start < end)
+    {
+        swap(v[start], v[end]);
+        start ++;
+        end --;
+    }
+    
+}
+
+int main()
+{
+
+    vector<char> arr = {'a', 'b', 'c', 'd', 'e'};
+    print_vector(arr);
+    reverse(arr.begin(), arr.end());
+    print_vector(arr);
+    reverse_string(arr);
+    print_vector(arr);
+
+    return 0;
+}
