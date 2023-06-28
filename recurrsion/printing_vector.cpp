a867f9c7f5a3346a4035edf82739078fe6b73ee6
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-06-28 12:10:44 +0530

    pushing

--- recurrsion/printing_vector.cpp
+++ recurrsion/printing_vector.cpp
@@ -14,23 +14,53 @@ void print_vector_using_loop(vector<int> &v){
 }
 
 // Approach 2: using recursion
-void print_vector_using_recursion(int n) {
+void print_vector_recursive(const vector<int>& vec, int index) {
+    if (index >= vec.size()) {
+        return;
+    }
+    
+    cout << vec[index] << " ";
+    print_vector_recursive(vec, index + 1);
+}
+
+// Function to print the number in ascending order till number n using recursion
+// tail recursion
+void print_using_recursion_in_ascending(int n) {
     if (n == 0) {
         return;
     }
     
-    print_vector_using_recursion(n - 1);
+    print_using_recursion_in_ascending(n - 1);
     cout << n << " ";
+}
+
+// Function to print the number in descending order
+// Head recursion
+void print_using_recursion_in_descending(int n) {
+    if (n == 0) {
+        return;
+    }
+    cout << n << " ";
+    print_using_recursion_in_descending(n - 1);
+    
 }
 
 
 int main(int argc, char const *argv[])
 {
     vector<int> v = {1,2,3,4,5,6,7,8};
+
+    // print the vector using loop
     print_vector_using_loop(v);
 
-
+    // printing vector using recursion
+    print_vector_recursive(v, 1);
+    cout<<endl;
+    
+    // n variable
     int n = 8;
-    print_vector_using_recursion(n);
+    
+    // print the number in ascending order
+    print_using_recursion_in_ascending(n);
     return 0;
 }
