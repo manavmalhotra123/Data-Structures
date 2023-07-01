1ca92e79c5238560173467eec88f5ae6b0455a2b
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-07-01 21:47:56 +0530

    linked list insert at end and insert at first position function

--- linked_lists/ll2.cpp
+++ linked_lists/ll2.cpp
@@ -1,57 +1,81 @@
-// taking the linked as input 
+// taking the linked as input
 
-#include<iostream>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 class Node
 {
-    public:
-        int data;
-        Node* next;
+public:
+    int data;
+    Node *next;
 
-        Node(){
-            this->data = 0;
-            this->next = NULL;
-        }
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
 
-        Node(int data){
-            this->data = data;
-            this->next = NULL;
-        }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
 };
 
-// insert a node at the head the of linked list 
-void InsertAtHead(Node* &head,int data){
+// insert a node at the head the of linked list
+void InsertAtHead(Node *&head, int data)
+{
     // Step 1
-    Node* newnode = new Node(data); // inserting the data in the node 
+    Node *newnode = new Node(data); // inserting the data in the node
     // Step 2
     newnode->next = head;
     // Step 3
     head = newnode;
 }
 
-// print function 
-void print(Node* &head) {
-    Node* temp = head;
-    while (temp != NULL) {
+// insert at tail or you can say at the end of the list
+// step 1: create a new node with the data - int data
+// step 2: join that new node with the last node of the linked list
+// insert the node at end of the linked list 
+void AtEnd(Node* &head, int data) {
+    Node* newnode = new Node(data);
+    if (head == NULL) {
+        head = newnode;
+    } else {
+        Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+        newnode->next = NULL;
+    }
+}
+
+
+// print function
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
         cout << temp->data << "-->";
         temp = temp->next;
     }
     cout << "NULL" << endl;
 }
 
-
 int main(int argc, char const *argv[])
 {
-    Node* head = NULL;
-    InsertAtHead(head,10);    
-    InsertAtHead(head,20);    
-    InsertAtHead(head,30);    
+    Node *head = NULL;
+    InsertAtHead(head, 10);
+    InsertAtHead(head, 20);
+    InsertAtHead(head, 30);
+    print(head);
+    AtEnd(head, 56);
     print(head);
 
-    
     return 0;
 }
