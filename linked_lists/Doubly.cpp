a40a8d63918587c9e145ef19f4b9b84551a3726a
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-07-01 22:50:12 +0530

    doubly linked lists

--- linked_lists/Doubly.cpp
+++ linked_lists/Doubly.cpp
@@ -2,19 +2,23 @@
 
 using namespace std;
 
+
+// Node class
 class Node
 {
     public:
-        int data;
-        Node* next;
-        Node* prev;
+        int data;   // data in the node
+        Node* next; // pointer to previous node
+        Node* prev; // pointer to next node
 
+        // constructor to initialize the node with 0 value
         Node(){
             this->data = 0;
             this->prev = NULL;
             this->next = NULL;
         }
-
+        
+        // constructor initialize the node with some value by user
         Node(int data){
             this->data = data;
             this->next = NULL;
@@ -25,7 +29,8 @@ class Node
 // Printing the doubly linked list
 void Display(Node* &head){
     Node* temp = head;
-
+    
+    // traversing the whole list 
     while (temp != NULL)
     {
         cout<<temp->data<<"-->";
@@ -48,6 +53,7 @@ int main()
     second->prev = first;
     second->next = third;
 
+    // function to display the doubly linked lists
     Display(head);
 
 
