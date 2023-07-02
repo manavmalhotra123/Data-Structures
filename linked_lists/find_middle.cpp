a960dc8c5e5d18d16dbea30c2447d901b0b99e38
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-07-02 16:57:15 +0530

    hare approach to find the middle node

--- linked_lists/find_middle.cpp
+++ linked_lists/find_middle.cpp
@@ -1,64 +1,88 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 class Node
 {
-    public:
-        int data;
-        Node* next;
+public:
+    int data;
+    Node *next;
 
-        Node(int data){
-            this->data = data;
-            this->next = NULL;
-        }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
 };
 
-void Show(Node* &head){
-    Node* temp = head;
+void Show(Node *&head)
+{
+    Node *temp = head;
     while (temp != NULL)
     {
-        cout<<temp->data<<"-->";
+        cout << temp->data << "-->";
         temp = temp->next;
     }
-    cout<<"NULL"<<endl;
+    cout << "NULL" << endl;
 }
 
-
-void MidNode(Node* &head){
+// Normal approach with O(N) time complexity
+void MidNode(Node *&head)
+{
     int length = 0;
     int mid = 0;
-    Node* temp = head;
+    Node *temp = head;
     while (temp != NULL)
     {
         length++;
         temp = temp->next;
     }
 
-    if (length%2 == 0)
+    if (length % 2 == 0)
     {
-        mid = length/2;
+        mid = length / 2;
     }
     else
     {
-        mid = (length/2) + 1;
+        mid = (length / 2) + 1;
     }
-    cout<<"Middle node is "<<mid<<endl;
-    
-    
+    cout << "Middle node is " << mid << endl;
 }
+Node* FindMiddleNode(Node* head) {
+    // If the list is empty
+    if (head == NULL) {
+        cout << "List is empty" << endl;
+        return NULL;
+    }
 
+    // If the list has only one node
+    if (head->next == NULL) {
+        return head;
+    }
 
+    // Initialize the tortoise and hare pointers
+    Node* tortoise = head;
+    Node* hare = head;
+
+    while (hare != NULL && hare->next != NULL) {
+        // Move the hare two steps
+        hare = hare->next->next;
+        // Move the tortoise one step
+        tortoise = tortoise->next;
+    }
+
+    return tortoise;
+}
 
 int main(int argc, char const *argv[])
 {
-    
-    Node* head = new Node(10);
-    Node* one = new Node(20);
-    Node* two = new Node(30);
-    Node* three = new Node(40);
-    Node* four = new Node(50);
-    Node* five = new Node(60);
+
+    Node *head = new Node(10);
+    Node *one = new Node(20);
+    Node *two = new Node(30);
+    Node *three = new Node(40);
+    Node *four = new Node(50);
+    Node *five = new Node(60);
 
     head->next = one;
     one->next = two;
@@ -68,7 +92,9 @@ int main(int argc, char const *argv[])
 
     Show(head);
 
-
     MidNode(head);
+
+
+    cout<<FindMiddleNode(head)->data<<endl;
     return 0;
 }
