bc8c110768e38ef3ec8bbe30e61bd6b124d961d4
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-07-04 10:59:24 +0530

    stack using array

--- stacks/array_stack.cpp
+++ stacks/array_stack.cpp
@@ -4,84 +4,90 @@ using namespace std;
 
 class Stack
 {
-    public:
-            int* arr;
-            int top;
-            int size;
+private:
+    int *arr;
+    int top;
+    int size;
 
-            Stack(int size){
-                arr = new int[size];
-                this->size = size;
-                top = -1;
-            }
+public:
+    Stack(int size)
+    {
+        arr = new int[size];
+        this->size = size;
+        top = -1;
+    }
 
-            // function 
-            void push(int data){
-                if (top == size - 1)
-                {
-                    cout<<"STACK OVERFLOW!!"<<endl;
-                    return;
-                }
-                
-                top++;
-                arr[top] = data;
-            }
+    // function
+    void push(int data)
+    {
+        if (top == size - 1)
+        {
+            cout << "STACK OVERFLOW!!" << endl;
+            return;
+        }
 
-            void pop(){
-                if (top == -1)
-                {
-                    cout<<"STACK UNDERFLOW!!, Nothing can be pop out..."<<endl;
-                    return;
-                }
-                
-                top --;
-                
-            }
+        top++;
+        arr[top] = data;
+    }
 
-            int Top(){
-                if (top == -1)
-                {
-                    cout<<"STACK IS EMPTY!!, Nothing is here.."<<endl;
-                    return -1;
-                }
-                
-                return arr[top];
-            }
+    void pop()
+    {
+        if (top == -1)
+        {
+            cout << "STACK UNDERFLOW!!, Nothing can be pop out..." << endl;
+            return;
+        }
 
-            int Size(){
-                return (top+1);
-            }
+        top--;
+    }
 
-            bool IsEmpty(){
-                if (top == -1)
-                {
-                    return true;
-                }
-                return false;
-                
-            }
+    int Top()
+    {
+        if (top == -1)
+        {
+            cout << "STACK IS EMPTY!!, Nothing is here.." << endl;
+            return -1;
+        }
 
-            void print(){
-                if (top == -1)
-                {
-                    cout<<"STACK IS EMPTY!!, Nothing is here.."<<endl;
-                    return;
-                }
-                else{
-                    cout<<"STACK"<<endl;
-                    while (top != -1)
-                    {
-                        cout<<arr[top]<<endl;
-                        top --;
-                    }
-                    cout<<endl;
-                }                
+        return arr[top];
+    }
+
+    int Size()
+    {
+        return (top + 1);
+    }
+
+    bool IsEmpty()
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void print()
+    {
+        if (top == -1)
+        {
+            cout << "STACK IS EMPTY!!, Nothing is here.." << endl;
+            return;
+        }
+        else
+        {
+            cout << "STACK" << endl;
+            while (top != -1)
+            {
+                cout << arr[top] << endl;
+                top--;
             }
+            cout << endl;
+        }
+    }
 };
-
 
 int main(int argc, char const *argv[])
 {
-    
+
     return 0;
 }
