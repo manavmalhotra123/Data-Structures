5fdecc6a7b161c90400a45b2a24949de17741d93
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-07-04 11:02:50 +0530

    stack using arrays

--- stacks/array_stack.cpp
+++ stacks/array_stack.cpp
@@ -2,53 +2,61 @@
 
 using namespace std;
 
+// Class Stack
 class Stack
 {
+// Inaccessible data members of the stack    
 private:
     int *arr;
     int top;
     int size;
 
 public:
+    // intializes the stack of elements = SIZE
     Stack(int size)
     {
         arr = new int[size];
         this->size = size;
-        top = -1;
+        top = -1; // initally the toop of the stack is -1 
     }
 
     // function
     void push(int data)
     {
+        // check whether the stack is full or not 
         if (top == size - 1)
         {
             cout << "STACK OVERFLOW!!" << endl;
             return;
         }
-
+        // if not full,then shift to upper position
         top++;
+        // insert data at upper position
         arr[top] = data;
     }
 
     void pop()
     {
+        // check whether the stack is empty or not
         if (top == -1)
         {
             cout << "STACK UNDERFLOW!!, Nothing can be pop out..." << endl;
             return;
         }
-
+        // top ko decrement krdo 
         top--;
     }
 
+    // accessing the top element of the stack
     int Top()
     {
+        // check for the empty values
         if (top == -1)
         {
             cout << "STACK IS EMPTY!!, Nothing is here.." << endl;
             return -1;
         }
-
+        // returning the value at the top of the stack
         return arr[top];
     }
 
