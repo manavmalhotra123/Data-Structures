f61c5a1c8983cf8547e2c79435f09794da5b63cc
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-07-06 12:59:53 +0530

    longest

--- stacks/longest_valid.cpp
+++ stacks/longest_valid.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int>st;
-        st.push(-1);
-        int len = 0;
-        for(int i=0; i<s.length(); i++){
+        stack<int> st; // Stack to store the indices of opening parentheses
+        st.push(-1); // Push -1 as the initial index to handle the case where the first character is a closing parenthesis
+        int len = 0; // Variable to store the length of the longest valid parentheses substring
+
+        // Iterate through each character of the input string
+        for (int i = 0; i < s.length(); i++) {
             char ch = s[i];
-            if(ch == '('){
-                st.push(i);
+
+            if (ch == '(') {
+                st.push(i); // Push the index of the opening parenthesis onto the stack
             }
-            else{
-                st.pop();
-                if(st.empty()){
-                    st.push(i);
+            else {
+                st.pop(); // Pop the top index from the stack as a matching closing parenthesis is found
+
+                if (st.empty()) {
+                    st.push(i); // If the stack becomes empty, push the current index to mark the start of a new potential valid parentheses substring
                 }
-                else{
-                    len = max(len, i-st.top());
+                else {
+                    len = max(len, i - st.top()); // Calculate the length of the current valid parentheses substring and update the maximum length if necessary
                 }
             }
         }
-        return len;
+
+        return len; // Return the length of the longest valid parentheses substring
     }
 };
