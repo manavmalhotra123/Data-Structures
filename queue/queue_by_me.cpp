f77b5ea903aa353102cbc337543c8ccdc3598233
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-07-07 13:04:09 +0530

    meri khud ki bnayi huyi queue

--- queue/queue_by_me.cpp
+++ queue/queue_by_me.cpp
@@ -2,78 +2,96 @@
 #include <vector>
 
 using namespace std;
-
 class Queue
 {
     private:
+            int* arr;
+            int size;
             int front;
             int rear;
-            int size;
-            int* arr;
     public:
-            Queue(int size)
-            {
-                arr = new int[size]; // queue of size is created 
-                // front and rear is equal to zero when the queue is empty 
+            Queue(int size){
+                this->size = size;
+                arr = new int[size];
                 front = 0;
                 rear = 0;
             }
-            void push(int value)
-            {
+
+            void Push(int data){
                 if (rear == size)
                 {
-                    return; // if full hai toh kya hi insert kroge ab yaar tum 
-                }
-                
-            }
-            void pop()
-            {
-                // check first whether the queue is empty or not 
-                if (front == rear)
-                {
+                    cout<<"Queue is full!"<<endl;
                     return;
                 }
-                
+                else
+                {
+                    arr[rear] = data;
+                    rear++;
+                }
             }
-            void GetFront()
-            {
-
+            void Pop(){
+                if (front == rear)
+                {
+                    cout<<"Queue is empty!"<<endl;
+                    return;
+                }
+                else
+                {
+                    arr[front] = -1;
+                    front++;
+                    // agar queue khaali hogi toh usko fir start pr le aayo nah
+                    if (front == rear)
+                    {
+                        front = 0;
+                        rear = 0;
+                    }
+                }
             }
-            void GetRear()
-            {
-
+            int GetFront(){
+                if (front == rear)
+                {
+                    cout<<"Queue is empty!"<<endl;
+                    return -1;
+                }
+                else
+                {
+                    return arr[front];
+                }
             }
-            void GetSize()
-            {
-
+            bool IsEmpty(){
+                if (front == rear)
+                {
+                    return true;
+                }
+                else
+                {
+                    false;
+                }
             }
+            int GetSize(){
+                return abs(front - rear );
+            }
+            friend void show(Queue Q);
 };
 
-void SHow(vector<int> v){
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout<<v[i]<<" ";
-    }
-    cout<<endl;
-    
-}
-
-
-void shift(vector<int> v){
-    for (int i = 0; i < v.size(); i++)
-    {
-        int temp = v[i+1];
-        v[i+1] = v[i];
-        v[i] = temp;
-    }
-    
+void show(Queue Q){
+  
 }
 
 int main(int argc, char const *argv[])
 {
-    vector<int> v = {1,2,3,4};
-    SHow(v);
-    shift(v);
-    SHow(v);
-    return 0;
+    Queue q(10);
+    q.Push(1);
+    q.Push(2);
+    q.Push(3);
+    q.Push(4);
+    q.Push(5);
+    q.Push(6);
+
+    cout<<"size of the queue is "<<q.GetSize()<<endl;
+
+    cout<<"front element is "<<q.GetFront()<<endl;    
+    q.Pop();
+    cout<<"front element is "<<q.GetFront()<<endl;
+    cout<<"size of the queue is "<<q.GetSize()<<endl;
 }
