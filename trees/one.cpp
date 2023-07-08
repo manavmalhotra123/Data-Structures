9050c99f7533771822a922f57e343c5d9e64b862
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-07-08 21:23:55 +0530

    my tree

--- trees/one.cpp
+++ trees/one.cpp
@@ -3,16 +3,51 @@
 It contains the nodes
 Binary Tree: number of children of one node can be 0,1 or 2
 N-ery Tree: n number of children of single node
+Skew Tree: one sides tree
+            10                         
+        20 
+    30
+40
+        OR
+10  
+    20 
+        30 
+            40
 
 Tree Terms;
 1.Node
-2.Root
-3.Child
-4.Parent
-5.Ancestor
-6.Sibling
-7.Leaf
+2.Root -- topmost node
+3.Child -- niche waala
+4.Parent -- upr waala ek ke 
+5.Ancestor -- do level upr
+6.Sibling -- sath ke level waala
+7.Leaf -- aakhri waaris family ka 
 8.Descendant
+*/
+
+/*
+Travesal techinque of the trees 
+1.Level order 
+2.Pre-order
+3.Post-order
+*/
+
+/*
+                      10 
+                20         30
+            40     50   60      70
+
+Level order traverse - 10  20  30  40   50   60   70 
+*/
+
+
+/*
+Build tree method:
+1. Level order traversal 
+2. Recursive traversal  - create a root node 
+                        - root ka left part recursion solve krega 
+                        - root ka right part recursion solve kreg 
+3. 
 */
 
 // Binary Tree
@@ -23,8 +58,13 @@ Tree Terms;
 
 using namespace std;
 
-
-
+class NryTree
+{
+private:
+    int data;
+    // for n nodes 
+    vector<NryTree*> child;
+};
 
 
 class BinaryNode
@@ -33,18 +73,8 @@ private:
     int data;
     BinaryNode *left;
     BinaryNode *right;
-public:
-    BinaryNode(){
-        data = 0;
-        left = NULL;
-        right = NULL;
-    }
-    BinaryNode(int data){
-        this->data = data;
-        left = NULL;
-        right = NULL;
-    }
 };
+
 
 int main(int argc, char const *argv[])
 {
--- trees/two.cpp
+++ trees/two.cpp
@@ -1,4 +1,4 @@
-// Creating a tree 
+// Creating a tree
 #include <iostream>
 #include <vector>
 #include <stack>
@@ -9,52 +9,52 @@ using namespace std;
 
 class TreeNode
 {
-        public:    
-            int data;
-            TreeNode* left;
-            TreeNode* right;
-            // node constructor
-            TreeNode(int data)
-            {
-                this->data = data;
-                right = NULL;
-                left = NULL;
-            }
+public:
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    // node constructor
+    TreeNode(int data)
+    {
+        this->data = data;
+        right = NULL;
+        left = NULL;
+    }
 };
 
-
-TreeNode* buildTree(){
+TreeNode *buildTree()
+{
     int data;
-    cout<<"Enter the data : ";
+    cout << "Enter the data : ";
     cin >> data;
 
-    // if data is -1 it means that you don't want to extend the tree 
+    // if data is -1 it means that you don't want to extend the tree
     if (data == -1)
     {
         return NULL;
     }
 
-    // ab kro aage ka kaam 
-    // Step 1: create a node 
-    TreeNode* root = new TreeNode(data);
-    cout<<"Enter data for left node of "<<data<<endl;
+    // ab kro aage ka kaam
+    // Step 1: create a node
+    TreeNode *root = new TreeNode(data);
+    cout << "Enter data for left node of " << data << endl;
     // Step 2: create a node at left
     root->left = buildTree();
     // Step 3: create a node at right
-    cout<<"Enter data for right node of "<<data<<endl;
+    cout << "Enter data for right node of " << data << endl;
     root->right = buildTree();
 
     return root;
 }
 
+void ShowTreeUsingLevelOrderTraversal(TreeNode *root)
+{
 
-void ShowTreeUsingLevelOrderTraversal(TreeNode* root){
+    cout << "Tree using level order traversal: ";
 
-    cout<<"Tree using level order traversal: ";
-
-    // initially ek queue li 
-    queue<TreeNode*> Q;
-    // queue mai humne root daal di tree ki 
+    // initially ek queue li
+    queue<TreeNode *> Q;
+    // queue mai humne root daal di tree ki
     Q.push(root);
 
     // till the queue is not empty
@@ -62,13 +62,14 @@ void ShowTreeUsingLevelOrderTraversal(TreeNode* root){
     {
 
         // Step 1: take the first element of the queue
-        TreeNode* temp = Q.front();
+        TreeNode *temp = Q.front();
         // Step 2: take out that bloody element
         Q.pop();
-        // Step 3: temp mai jo node aaya uske data ko print maaro 
-        cout<<temp->data<<" ";
+        // Step 3: temp mai jo node aaya uske data ko print maaro
+        cout << temp->data << " ";
         // Step 4: check for children at left and right if exist then push them to queue
         if (temp->left)
+
         {
             Q.push(temp->left);
         }
@@ -77,16 +78,13 @@ void ShowTreeUsingLevelOrderTraversal(TreeNode* root){
             Q.push(temp->right);
         }
     }
-    cout<<endl;
-    
+    cout << endl;
 }
-
-
 
 int main(int argc, char const *argv[])
 {
-   
-    TreeNode* root = buildTree();
-    ShowTreeUsingLevelOrderTraversal(root);   
+
+    TreeNode *root = buildTree();
+    ShowTreeUsingLevelOrderTraversal(root);
     return 0;
 }
