86f443d4403293dc6318c78abcf807daaea02a85
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-07-13 12:01:54 +0530

    k the ancestor

--- trees/k_anscestor.cpp
+++ trees/k_anscestor.cpp
@@ -18,60 +18,32 @@ public:
     }
 };
 
+
+
 // Function to find the kth ancestor of a node in a binary tree
-int kthAncestor(TreeNode *root, TreeNode *target, int k)
+bool KthAncestor(TreeNode* root, int k, TreeNode* p)
 {
-    stack<TreeNode *> ancestors;
-    bool found = false;
-
-    // Perform DFS to find the target node
-    TreeNode *current = root;
-    while (current || !ancestors.empty())
+    // base case
+    if (root == nullptr)
     {
-        while (current)
-        {
-            ancestors.push(current);
-            if (current == target)
-            {
-                found = true;
-                break;
-            }
-            current = current->left;
-        }
-
-        if (found)
-            break;
-
-        current = ancestors.top();
-        if (current->right)
-        {
-            current = current->right;
-        }
-        else
-        {
-            ancestors.pop();
-            while (!ancestors.empty() && ancestors.top()->right == current)
-            {
-                current = ancestors.top();
-                ancestors.pop();
-            }
-            current = ancestors.empty() ? nullptr : ancestors.top()->right;
-        }
+        return false;
     }
 
-    // Check if enough ancestors are present
-    if (ancestors.size() < k)
-        return -1;
-
-    // Pop the top k-1 elements from the stack to get the kth ancestor
-    for (int i = 0; i < k - 1; i++)
+    if (root == p || KthAncestor(root->left, k, p) || KthAncestor(root->right, k, p))
     {
-        ancestors.pop();
+        if (k == 0)
+        {
+            cout << "Kth ancestor is " << root->data << endl;
+        }
+        k--;
+        return true;
     }
 
-    // Return the value of the kth ancestor
-    return ancestors.top()->data;
+    return false;
 }
+
+
+
 
 int main()
 {
@@ -88,12 +60,7 @@ int main()
     TreeNode *target = root->left->right; // Node with value 5
     int k = 2;
 
-    int kthAncestorValue = kthAncestor(root, target, k);
-
-    if (kthAncestorValue != -1)
-        cout << "The " << k << "th ancestor of node " << target->data << " is " << kthAncestorValue << endl;
-    else
-        cout << "There is no " << k << "th ancestor of node " << target->data << endl;
+    bool result = KthAncestor(root,k,target);
 
     return 0;
 }
