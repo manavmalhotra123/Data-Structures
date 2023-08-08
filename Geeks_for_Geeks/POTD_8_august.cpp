c0b61dc1babb96bbbe2e50caf54c881a8dfe5e15
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-08-08 10:58:01 +0530

    POTD 8 august 2023

--- Geeks_for_Geeks/POTD_8_august.cpp
+++ Geeks_for_Geeks/POTD_8_august.cpp
@@ -7,50 +7,42 @@
 
 using namespace std;
 
-class solution
-{
-    public:
-        int countfractions(int n, int numerator[], int denominator[])
-        {
-            const int MAX_DENOMINATOR = 10000;
-            int fractionCount[MAX_DENOMINATOR][2] = {0};
-            int answer = 0;
+class Solution {
+public:
+    int countFractions(int n, int numerator[], int denominator[]) {
+        const int MAX_DENOMINATOR = 10000;
+        int fractionCount[MAX_DENOMINATOR][2] = {0}; // Stores counts of unique fraction pairs
+        int answer = 0; // The final count of pairs whose sum equals 1
 
-            for (int i = 0; i < n; i++)
-            {
-                int x = numerator[i];
-                int y = denominator[i];
-                int gcdValue = __gcd(x, y);
-                x /= gcdValue;
-                y /= gcdValue;
-                int want_x = y - x;
-                int want_y = y;
+        for (int i = 0; i < n; i++) {
+            int x = numerator[i];
+            int y = denominator[i];
+            int gcdValue = __gcd(x, y); // Calculate the greatest common divisor
+            x /= gcdValue;
+            y /= gcdValue;
+            int want_x = y - x; // The complement numerator
+            int want_y = y; // The complement denominator
 
-                if (want_y < MAX_DENOMINATOR)
-                {
-                    answer += fractionCount[want_y][want_x];
-                }
-                
-                fractionCount[y][x]++;
+            if (want_y < MAX_DENOMINATOR) {
+                // If the complement denominator is within the bounds of MAX_DENOMINATOR
+                // Add the count of pairs with the complement fraction to the answer
+                answer += fractionCount[want_y][want_x];
             }
-            
-            return answer;
+
+            fractionCount[y][x]++; // Increment the count for the current fraction
         }
+
+        return answer; // Return the total count of pairs
+    }
 };
 
-
-
-
-int main(int argc, char const *argv[])
-{
-    solution sol;
+int main(int argc, char const *argv[]) {
+    Solution sol;
     int n = 4;
     int numerator[] = {1, 2, 3, 4};
     int denominator[] = {6, 3, 4, 12};
-    int result = sol.countfractions(n, numerator, denominator);
+    int result = sol.countFractions(n, numerator, denominator);
     cout << "Count of pairs: " << result << endl;
 
-    return 0;
-
-    return 0;
+    return 0; // Exit the program
 }
